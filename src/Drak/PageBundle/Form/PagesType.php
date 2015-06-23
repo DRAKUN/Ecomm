cb96b7636d7b8b6952632a251daacacacf869e5c
Drakun <dj-tung@hotmail.com>
2015-06-23 10:34:35 +0000

    pages mentions legales

---
+++ src/Drak/PageBundle/Form/PagesType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drak\PageBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PagesType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('contenu')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Drak\PageBundle\Entity\Pages'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'drak_pagebundle_pages';
+    }
+}
